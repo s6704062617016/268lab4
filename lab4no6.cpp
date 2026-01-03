48161fef766720c3872dbc362c8c81b1df923c55
s6704062617016 <s6704062617016@email.kmutnb.ac.th>
2026-01-04 00:05:49 +0700

    Commit 3>> lab4no6.c display square of a character

---
+++ lab4no6.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main (void){
+	char ch;
+	int i,n;
+	scanf(" %c %d", &ch,&n);
+	for(i=0;i<n;i++){
+	for (int j = 0; j<n; j++)
+	printf("%c", ch);
+	printf("\n");
+} 
+return 0;
+} 
