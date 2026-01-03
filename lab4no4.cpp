62e19f262cfaea9fc43f1f143269f03786eb5a31
s6704062617016 <s6704062617016@email.kmutnb.ac.th>
2026-01-04 00:03:41 +0700

    Commit 1>> lab4no4.c nested loop

---
+++ lab4no4.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main (void){
+int i = 1,j; 
+while (i <= 3){
+   printf("Row %d: ", i);
+   for(j=1;j<=5;j++){
+            printf("%3d ",j);
+        }
+   printf("\n");
+   i++ ;
+} 
+return 0;
+} 
